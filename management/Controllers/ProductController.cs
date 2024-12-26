7e1b3e61b15d340808abfe3038f18ba87afe15c6
thethtar <thethtarsann@fellowship-corp.com>
2024-12-26 11:00:07 +0900

    Add project files.

---
+++ management/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using management.Data;
+using management.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace management.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetMyModels()
+        {
+            return await _context.Products.ToListAsync();
+        }
+      
+
+    }
+
+}
